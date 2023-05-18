4a0186e221b26f576f42d6fd20233c353ab736ff
Austin Henrie <nitsua15@gmail.com>
2023-05-18 09:39:07 -0600

    fixed axios request

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 document.querySelector("#character").addEventListener("click", function() {
-    axios.get("http://localhost:3000/character").then(function(response) {
+    axios.get("/character").then(function(response) {
         alert("The protagonist's name is " + response.data + "!")
     })
 })
