69db1643e932fe932a913574464c36dc144cc836
Austin Henrie <nitsua15@gmail.com>
2023-05-18 08:27:56 -0600

    removed logs

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 let express = require("express")
 let api = express()
 api.use(require("cors")())
-console.log(__dirname + "/public")
 api.use(express.static(__dirname + "/public"))
 api.get("/character", function(request, response) {
-    console.log(response.data)
     response.send("Gavin Guile")
 })
 api.listen(3000, function() {
