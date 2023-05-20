0da2bdc1f27ac6c517deac12d01266eeb8bd2850
Austin Henrie <nitsua15@gmail.com>
2023-05-19 22:12:25 -0600

    randomized protagonist name

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
 let Rollbar = require("rollbar")
-let rollbar = new Rollbar({
-    "accessToken": "e6209aa6a1b14368b5fca369c5780951",
-    "captureUncaught": true,
-    "captureUnhandledRejections": true
-})
+let rollbar = new Rollbar({ "accessToken": "e6209aa6a1b14368b5fca369c5780951", "captureUncaught": true, "captureUnhandledRejections": true })
 let express = require("express")
 let sequelize = require("sequelize")
-let secrets = require("./secrets")
 let api = express()
-let database = new sequelize(secrets.key, {
+api.use(express.static(__dirname + "/public"))
+let database = new sequelize(require("./secrets").key, {
     "dialect": "postgres",
     "dialectOptions": {
-        "ssl": {
-            "rejectUnauthorized": false
-        }
+        "ssl": { "rejectUnauthorized": false }
     }
 })
-api.use(express.static(__dirname + "/public"))
 api.get("/character", function(request, response) {
     database.query("select * from characters").then(function(selection) {
-        response.send(selection[0][0].name)
+        if (!selection) {
+            rollbar.error("problem with getting protagonist")
+        }
+        response.send(selection[0][0].name.split(" ")[Math.floor(2 * Math.random())])
     })
 })
-// api.get("/author", function(request, response) {
-//     response.send("Brent Weeks")
-// })
-console.log(rollbar.options.host)
 api.get("/author", function() {
     try {
         respond()
